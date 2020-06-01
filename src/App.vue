eab6e1001f95831d88bdffd9a2826e6e61168b60
ZermattChris <chris@flyzermatt.com>
2020-06-01 18:08:10 +0200

    Some rough layout working

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,64 @@
 <template>
-  <div id="app">
-    <p>This is the App Shell</p>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div 
+        id="logo"
+        class="d-flex align-center"
+      >
+        <v-img
+          alt="FlyZermatt Logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/FlyZermatt-logo-light.svg"
+          transition="scale-transition"
+          width="220"
+        />
+      </div>
+
+    </v-app-bar>
+
+    <v-content>
+      <v-row no-gutters>
+        <v-col cols="12" sm="4" style="background-color:yellow;"></v-col>
+        <v-col cols="12" sm="4" style="background-color:silver;">
+          <router-view></router-view>
+        </v-col>
+        <v-col cols="12" sm="4" style="background-color:yellow;"></v-col>
+      </v-row>
+    </v-content>
+
+    <v-footer
+      color="primary"
+      app
+    >
+      <span class="white--text">Not sure what to put here, but like the fixed footer</span>
+    </v-footer>
+  </v-app>
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
 <style>
+
+#logo {
+  margin: 0 auto;
+}
 
 </style>
--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <p>home</p>
+    <p>Start - Step 1</p>
   </div>
 </template>
 
@@ -8,7 +8,7 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
+  name: 'Start',
   components: {
       
   }
