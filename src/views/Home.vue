f3f25afa09ddb1cc5f3e63ba2d237d0f73b4430e
ZermattChris <chris@flyzermatt.com>
2020-06-01 17:35:13 +0200

    Tweaks before adding Vuetify

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <p>home</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+      
+  }
+}
+</script>
