a23e01548d518e579509fc815144459591c13070
ZermattChris <chris@flyzermatt.com>
2020-06-08 14:05:19 +0200

    ...

--- src/components/TimeLister.vue
+++ src/components/TimeLister.vue
@@ -18,21 +18,21 @@
         v-for="(item, i) in items"
         :key="i"
       >
-        <!-- <v-list-item-icon>
+        <v-list-item-icon>
           <v-icon v-text="tmpIcon"></v-icon>
-        </v-list-item-icon> -->
+        </v-list-item-icon>
 
         <v-list-item-content>
           <v-list-item-title v-text="item.time + ' X Places Available'"></v-list-item-title>
         </v-list-item-content>
 
-        <!-- <v-list-item-action>
+        <v-list-item-action>
           <v-switch 
             inset 
             :dense="isDense"
             color="success"
           ></v-switch>
-        </v-list-item-action> -->
+        </v-list-item-action>
 
       </v-list-item>
 
