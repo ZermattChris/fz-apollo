e94db6cf718a535cfbf6f2b87a6db9b9dfb9b550
Chris Banford <chris@bsoft.ch>
2020-10-14 13:54:20 +0200

    Added rough Slide Group below current solution

--- src/views/Step_TimeSlot.vue
+++ src/views/Step_TimeSlot.vue
@@ -20,6 +20,72 @@
     otherwise any contents after the TLG would jump up. -->
     <div class="vSpacerForAbsolute"></div>
 
+
+
+
+<!-- start of new timelist object test -->
+
+
+    <v-sheet
+    class="mx-auto"
+    elevation="8"
+    max-width="800"
+  >
+    <v-slide-group
+      v-model="model"
+      class="pa-4"
+      show-arrows
+      center-active
+    >
+      <v-slide-item
+        v-for="n in 15"
+        :key="n"
+        v-slot:default="{ active, toggle }"
+      >
+        <v-card
+          :color="active ? 'primary' : 'grey lighten-1'"
+          class="ma-4"
+          height="200"
+          width="100"
+          @click="toggle"
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <v-scale-transition>
+              <v-icon
+                v-if="active"
+                color="white"
+                size="48"
+                v-text="'mdi-close-circle-outline'"
+              ></v-icon>
+            </v-scale-transition>
+          </v-row>
+        </v-card>
+      </v-slide-item>
+    </v-slide-group>
+
+    <v-expand-transition>
+      <v-sheet
+        v-if="model != null"
+        height="200"
+        tile
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <h3 class="title">
+            Selected {{ model }}
+          </h3>
+        </v-row>
+      </v-sheet>
+    </v-expand-transition>
+  </v-sheet>
+
   </div>
 </template>
 
@@ -39,7 +105,7 @@
 
     data () {
       return {
-        
+        model: null,
       }
     },
 
